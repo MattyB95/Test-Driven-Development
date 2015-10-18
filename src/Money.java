92d1471f363643859f6697a9516e177f2c56fe5a
Matthew Boakes <Matthew.Boakes@Gmail.com>
2015-10-18 21:20:35 +0100

    Chapter 9: Times We're Livin' In

--- src/Money.java
+++ src/Money.java
@@ -5,13 +5,19 @@
 abstract public class Money {
 
     protected int amount;
+    protected String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
 
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     abstract Money times(int multiplier);
@@ -21,4 +27,7 @@ abstract public class Money {
         return amount == money.amount && getClass().equals(money.getClass());
     }
 
+    String currency() {
+        return currency;
+    }
 }
