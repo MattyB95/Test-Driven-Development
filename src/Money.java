f135709143a955d0b5754d3fd8f12ed536b34f12
Matthew Boakes <Matthew.Boakes@Gmail.com>
2015-10-18 21:01:04 +0100

    Chapter 8: Makin' Objects

--- src/Money.java
+++ src/Money.java
@@ -2,9 +2,19 @@
  * Created by Matthew on 18/10/2015.
  * Test-Drive Development By Example.
  */
-public class Money {
+abstract public class Money {
 
     protected int amount;
+
+    static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    abstract Money times(int multiplier);
 
     public boolean equals(Object object) {
         Money money = (Money) object;
