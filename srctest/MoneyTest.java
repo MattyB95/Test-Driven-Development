f825eb5b0774b081013068a15c5416358512b0ea
Matthew Boakes <Matthew.Boakes@Gmail.com>
2015-10-25 15:10:46 +0000

    Chapter 16: Abstraction, Finally

--- srctest/MoneyTest.java
+++ srctest/MoneyTest.java
@@ -71,11 +71,6 @@ public class MoneyTest {
     }
 
     @Test
-    public void testArrayEquals() {
-        assertEquals(new Object[] {"abc"}, new Object[] {"abc"});
-    }
-
-    @Test
     public void testIdentityRate() {
         assertEquals(1, new Bank().rate("USD", "USD"));
     }
@@ -88,6 +83,28 @@ public class MoneyTest {
         bank.addRate("CHF", "USD", 2);
         Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
         assertEquals(Money.dollar(10), result);
+    }
+
+    @Test
+    public void testSumPlusMoney() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFrancs).plus(fiveBucks);
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(15), result);
+    }
+
+    @Test
+    public void testSumTimes() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFranc = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFranc).times(2);
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(Money.dollar(20), result);
     }
 
 }
